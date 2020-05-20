61d015abda61aa08a4ec99031b3fb3b11a7703c3
Amandeep J Baryah <abaryah@wes.org>
2020-05-20 12:15:49 -0400

    added more test

--- Program.cs
+++ Program.cs
@@ -12,7 +12,9 @@ namespace ExtractRefNumber.App
                 "3456782.pdf",
                 "3456789000.5543542.pdf",
                 "unknwon.kusdfh.4567890.5678933.pdf",
-                ""
+                "ULS-WES 4564838-1.pdf",
+                "MU-.pdf",
+                "app-4459316-ee079896-9080-449c-82dc-210078b7b982.tiff"
             };
             
             foreach(var str in strs)
